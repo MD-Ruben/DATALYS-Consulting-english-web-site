97e9081dd4cbc632899d851c6958dad86f701864
yab21 <yablaiyablairubenvirgil@gmail.com>
2025-01-27 11:16:08 +0000

    reglages du soucis du slider

--- src/components/LayoutEN/LastHero/index.tsx
+++ src/components/LayoutEN/LastHero/index.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import React, { useState, useEffect } from "react"
+import { motion, AnimatePresence } from "framer-motion"
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa"
 
 const LastHero = () => {
   const slides = [
@@ -15,8 +15,8 @@ const LastHero = () => {
       description: "Today, data is a growth driver for many companies.",
       buttons: [
         { text: "Talk to an expert", link: "/contact" },
-        { text: "Ask for a quote", link: "/contact" }
-      ]
+        { text: "Ask for a quote", link: "/contact" },
+      ],
     },
     {
       id: 2,
@@ -26,8 +26,8 @@ const LastHero = () => {
       description: "Today, data is a growth driver for many companies.",
       buttons: [
         { text: "Talk to an expert", link: "/contact" },
-        { text: "Ask for a quote", link: "/contact" }
-      ]
+        { text: "Ask for a quote", link: "/contact" },
+      ],
     },
     {
       id: 3,
@@ -37,8 +37,8 @@ const LastHero = () => {
       description: "Today, data is a growth driver for many companies.",
       buttons: [
         { text: "Talk to an expert", link: "/contact" },
-        { text: "Ask for a quote", link: "/contact" }
-      ]
+        { text: "Ask for a quote", link: "/contact" },
+      ],
     },
     {
       id: 4,
@@ -48,77 +48,81 @@ const LastHero = () => {
       description: "Today, data is a growth driver for many companies.",
       buttons: [
         { text: "Talk to an expert", link: "/contact" },
-        { text: "Ask for a quote", link: "/contact" }
-      ]
+        { text: "Ask for a quote", link: "/contact" },
+      ],
     },
     {
-        id: 5,
-        image: "/images/slider/new/reseau&securite2.jpg",
-        title2: "DATALYS",
+      id: 5,
+      image: "/images/slider/new/reseau&securite2.jpg",
+      title2: "DATALYS",
       title3: "Network & Security",
-        description: "Today, data is a growth driver for many companies.",
-        buttons: [
-          { text: "Talk to an expert", link: "/contact" },
-          { text: "Ask for a quote", link: "/contact" }
-        ]
-      }
-  ];
+      description: "Today, data is a growth driver for many companies.",
+      buttons: [
+        { text: "Talk to an expert", link: "/contact" },
+        { text: "Ask for a quote", link: "/contact" },
+      ],
+    },
+  ]
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [isAutoPlaying, setIsAutoPlaying] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [isAutoPlaying, setIsAutoPlaying] = useState(true)
 
   useEffect(() => {
-    let interval;
+    let interval
     if (isAutoPlaying) {
       interval = setInterval(() => {
-        nextSlide();
-      }, 5000);
+        nextSlide()
+      }, 5000)
     }
-    return () => clearInterval(interval);
-  }, [currentIndex, isAutoPlaying]);
+    return () => clearInterval(interval)
+  }, [currentIndex, isAutoPlaying])
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === slides.length - 1 ? 0 : prevIndex + 1
-    );
-  };
+      prevIndex === slides.length - 1 ? 0 : prevIndex + 1,
+    )
+  }
 
   const prevSlide = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? slides.length - 1 : prevIndex - 1
-    );
-  };
+      prevIndex === 0 ? slides.length - 1 : prevIndex - 1,
+    )
+  }
 
   const goToSlide = (index) => {
-    setCurrentIndex(index);
-  };
+    setCurrentIndex(index)
+  }
 
   return (
     <div
-      className="relative pt-20 md:pt-36 w-full h-[650px] md:h-[700px] lg:h-[800px] overflow-hidden"
+      className="relative h-[650px] w-full overflow-hidden pt-20 md:h-[700px] md:pt-36 lg:h-[800px]"
       onMouseEnter={() => setIsAutoPlaying(false)}
       onMouseLeave={() => setIsAutoPlaying(true)}
     >
-      <AnimatePresence initial={false} custom={currentIndex}>
+      <AnimatePresence mode="wait" initial={false}>
         <motion.div
           key={currentIndex}
-          initial={{ opacity: 0, x: 1000 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -1000 }}
-          transition={{ duration: 0.5 }}
-          className="absolute w-full h-full"
+          initial={{ opacity: 0.5 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0.5 }}
+          transition={{ duration: 0.7 }}
+          className="absolute h-full w-full"
         >
           <div
-            className="w-full h-full bg-cover bg-center relative"
-            style={{ backgroundImage: `url(${slides[currentIndex].image})` }}
+            className="relative h-full w-full bg-cover bg-center bg-no-repeat transition-all duration-700"
+            style={{
+              backgroundImage: `url(${slides[currentIndex].image})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
           >
             <div className="absolute inset-0 bg-black bg-opacity-40">
-              <div className="flex flex-col items-center justify-center text-center h-full text-white px-4 -mt-14">
+              <div className="-mt-14 flex h-full flex-col items-center justify-center px-4 text-center text-white">
                 <motion.h2
                   initial={{ opacity: 0, y: 20 }}
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ delay: 0.3 }}
-                  className="pointer-events-none bg-clip-text text-3xl font-extralight leading-tight text-transparent dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight lg:text-6xl lg:tracking-wide mb-2"
+                  className="pointer-events-none mb-2 bg-clip-text text-3xl font-extralight leading-tight text-transparent dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight lg:text-6xl lg:tracking-wide"
                 >
                   {slides[currentIndex].title}
                 </motion.h2>
@@ -126,15 +130,18 @@ const LastHero = () => {
                   initial={{ opacity: 0, y: 20 }}
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ delay: 0.5 }}
-                  className="text-3xl font-extralight leading-tight dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight lg:text-6xl lg:font-bold lg:tracking-wide mb-2"
+                  className="mb-2 text-3xl font-extralight leading-tight dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight lg:text-6xl lg:font-bold lg:tracking-wide"
                 >
-                  {slides[currentIndex].title2} <span className="font-extralight leading-tight text-[#f5c034]">Consulting</span>
+                  {slides[currentIndex].title2}{" "}
+                  <span className="font-extralight leading-tight text-[#f5c034]">
+                    Consulting
+                  </span>
                 </motion.h2>
                 <motion.h3
                   initial={{ opacity: 0, y: 20 }}
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ delay: 0.5 }}
-                  className="pointer-events-none bg-clip-text text-xl font-semibold capitalize leading-tight text-transparent dark:text-white sm:text-2xl md:text-3xl lg:text-4xl mb-2"
+                  className="pointer-events-none mb-2 bg-clip-text text-xl font-semibold capitalize leading-tight text-transparent dark:text-white sm:text-2xl md:text-3xl lg:text-4xl"
                 >
                   {slides[currentIndex].title3}
                 </motion.h3>
@@ -150,13 +157,13 @@ const LastHero = () => {
                   initial={{ opacity: 0, y: 20 }}
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ delay: 0.7 }}
-                  className="flex flex-col sm:flex-row gap-4"
+                  className="flex flex-col gap-4 sm:flex-row"
                 >
                   {slides[currentIndex].buttons.map((button, index) => (
                     <a
                       key={index}
                       href={button.link}
-                      className={`px-6 py-3 rounded-full text-sm md:text-base transition-all duration-300 ${
+                      className={`rounded-full px-6 py-3 text-sm transition-all duration-300 md:text-base ${
                         index === 0
                           ? "bg-[#f5c034] text-white hover:bg-opacity-90"
                           : "border-2 border-white hover:bg-white hover:text-black"
@@ -188,14 +195,14 @@ const LastHero = () => {
         <FaChevronRight className="text-2xl text-gray-800" />
       </button> */}
 
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex space-x-4">
+      <div className="absolute bottom-8 left-1/2 flex -translate-x-1/2 transform space-x-4">
         {slides.map((_, index) => (
           <button
             key={index}
             onClick={() => goToSlide(index)}
             className={`h-1 transition-all duration-300 ${
-              index === currentIndex 
-                ? "w-8 bg-white" 
+              index === currentIndex
+                ? "w-8 bg-white"
                 : "w-4 bg-white/50 hover:bg-white/75"
             }`}
             aria-label={`Go to slide ${index + 1}`}
@@ -203,7 +210,7 @@ const LastHero = () => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LastHero;
+export default LastHero
